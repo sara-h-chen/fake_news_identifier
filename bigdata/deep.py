041a95937faac0bcc28fe8ab76e3bec14dc387a3
sara-h-chen <deutscher71112@gmail.com>
2018-03-07 02:57:32 +0000

    Complete LSTM

--- bigdata/deep.py
+++ bigdata/deep.py
@@ -1,42 +1,84 @@
 import pandas
 import re
 import numpy as np
+import spacy
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
-import spacy
+
+from keras import optimizers
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers.embeddings import Embedding
 from keras.preprocessing.sequence import pad_sequences
 
 # Set seed for reproducability
 np.random.seed(1337)
 
-def string_to_wordvec(string, nlp):
-	tokens = nlp(string)
-	return np.array([token.vector for token in tokens])
+# Load spaCy once
+nlp = spacy.load('en_core_web_md')
 
-def process_data(pairs, nlp):
+class MetricsCallback(Callback):
+    def __init__(self, train_data, validation_data):
+        super().__init__()
+        self.validation_data = validation_data
+        self.train_data = train_data
+
+    def on_epoch_end(self, epoch, logs={}):
+        X_train = self.train_data[0]
+        y_train = self.train_data[1]
+
+        X_val = self.validation_data[0]
+        y_val = self.validation_data[1]
+
+        accuracy = met.accuracy_score(y_test, y_pred)
+		precision = met.precision_score(y_test, y_pred)
+		recall = met.recall_score(y_test, y_pred)
+		F1 = met.f1_score(y_test, y_pred)
+		print("Accuracy: {0}, Precision: {1}, Recall: {2}, F1: {3}".format(accuracy, precision, recall, F1))
+
+class LemmaTokenizer(object):
+	def __init__(self):
+		self.spacynlp = nlp
+		
+	def __call__(self, doc):
+		nlpdoc = self.spacynlp(doc)
+		nlpdoc = [token.lemma_ for token in nlpdoc if (len(token.lemma_) > 1) or(token.lemma_.isalnum())]
+		return nlpdoc
+
+def string_to_wordvec(string, token):
+	tokens = token(string)
+	return np.array([nlp(token).vector for token in tokens])
+
+def vectorize_words(pairs, label_pairs, tokenizer):
 	word_vecs = []
-	for id_num, string in pairs:
-		word_vecs.append(string_to_wordvec(string, nlp))
+	label_after_vec = []
+	paired_up = zip(pairs, label_pairs)
+	# List of ((id, string),(id, label))
+	for text_tuple, label_tuple in paired_up:
+		paragraphs = text_tuple[1].split("\n")
+		for paragraph in paragraphs:
+			word_vecs.append(string_to_wordvec(paragraph, tokenizer))
+			label_after_vec.append(label_tuple[1])
 		print(len(word_vecs))
-	return word_vecs
+	return word_vecs, label_after_vec
+
 
 ### INITIAL
-
-# TODO: Change this to weblarge
-nlp = spacy.load('en', vectors='en_glove_cc_300_1m')
-
 df = pandas.read_csv('data/news_ds.csv')
+# Remove HTML tags, digits, punctuation
+df['CLEAN'] = df['TEXT'].str.replace('<[^<]+?>|^\d+\s|\s\d+\s|\s\d+$|[^\w\s]', '')
 
 # Work with a small subset
-num_sample = 40
-raw_data = df['TEXT'][:num_sample]
+num_sample = 30
+raw_data = df['CLEAN'][:num_sample]
 raw_labels = df['LABEL'][:num_sample]
 
-data = process_data(raw_data.items(), nlp)
-labels = np.array([x[1] for x in raw_labels.items()])
+lemma_token = LemmaTokenizer()
+data, labels = vectorize_words(raw_data.items(), raw_labels.items(), lemma_token)
 
 # Truncate longer to 1000 and pad shorter to 1000
 data = pad_sequences(data, maxlen=1000, padding='post')
@@ -54,23 +96,31 @@ dataset_size, num_timesteps, num_dimensions = X_train.shape
 
 # Create model
 model = Sequential()
+model.add(LSTM(20, input_shape=(num_timesteps, num_dimensions), activation='relu', return_sequences=True))
+model.add(Dense(10, activation='relu'))
+model.add(Dropout(0.2))
 model.add(LSTM(10, input_shape=(num_timesteps, num_dimensions), activation='relu'))
+# model.add(Dropout(0.1))
 model.add(Dense(1, activation='sigmoid'))
 # Loss is t * log(y) + (1 - t) * log (1 - y)
-model.compile(loss='binary_crossentropy', optimizer='sgd')
+sgd = optimizers.SGD(lr=0.01, clipvalue=0.5)
+model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 print("Fitting")
-model.fit(X_train, y_train, epochs=10, batch_size=1, verbose=2, validation_data=[X_val, y_val])
+metrics_callback = MetricsCallback(train_data=(X_train, y_train), validation_data=(X_val, y_val))
+model.fit(X_train, y_train, epochs=3, batch_size=1, verbose=2, validation_data=[X_val, y_val])
 
 print("Predicting")
 
+scores = model.evaluate(X_val, y_val, verbose=0)
+print('Accuracy: %.5f' % (scores[1]*100))
 # Check validation score
 # TODO: Check if positives means fake(0) or real(1)
-y_pred = np.round(model.predict(X_val))
-cf_matrix = confusion_matrix(y_pred, y_val)
-tn, fn, fp, tp = cf_matrix.flatten()
-accuracy = (tp + tn) / (tn + fp + fn + tp)
-precision = tp / (tp + fp)
-recall = tp / (tp + fn)
-F1 = 2 * tp / (2 * tp + fp + fn)
-print("Accuracy: {0}, Precision: {1}, Recall: {2}, F1: {3}".format(accuracy, precision, recall, F1))
+# y_pred = np.round(model.predict(X_val))
+# cf_matrix = confusion_matrix(y_pred, y_val)
+# tn, fn, fp, tp = cf_matrix.flatten()
+# accuracy = (tp + tn) / (tn + fp + fn + tp)
+# precision = tp / (tp + fp)
+# recall = tp / (tp + fn)
+# F1 = 2 * tp / (2 * tp + fp + fn)
+# print("Accuracy: {0}, Precision: {1}, Recall: {2}, F1: {3}".format(accuracy, precision, recall, F1))
--- bigdata/shallow.py
+++ bigdata/shallow.py
@@ -11,7 +11,6 @@ from nltk import WordNetLemmatizer
 from nltk import sent_tokenize
 from nltk import pos_tag
 
-from collections import Counter
 from spacy.lang.en.stop_words import STOP_WORDS
 
 from scipy.sparse.csr import csr_matrix
@@ -28,71 +27,19 @@ class LemmaTokenizer(object):
 		nlpdoc = [token.lemma_ for token in nlpdoc if (len(token.lemma_) > 1) or(token.lemma_.isalnum())]
 		return nlpdoc
 
-# TODO: Clean data
-# def get_tokens(string):
-# 	return [x.lower() for x in re.split("\W+", string) if x]
-
-# def identify_tokens(pairs):
-# 	tokens = set()
-# 	for id_num, string in pairs:
-# 		new_tokens = get_tokens(string)
-# 		tokens = tokens.union(new_tokens)
-# 	return sorted(tokens)
-
-# def count_tokens(pairs, unique_tokens):
-# 	tfs = []
-# 	for id_num, string in pairs:
-# 		token_counter = Counter()
-# 		token_counter.update(get_tokens(string))
-# 		tf_counts = list(map(lambda el: token_counter[el], unique_tokens))
-# 		tfs.append(tf_counts)
-# 		print(len(tfs))
-# 	return np.array(tfs)
-
-# def save_tokens(pairs, tokens, fname):
-# 	f = open(fname, 'w')
-# 	f.write("ID" + ",".join(tokens) + "\n")
-# 	for id_num, string in pairs:
-# 		print(id_num)
-# 		token_counter = Counter()
-# 		token_counter.update(get_tokens(string))
-# 		tf_counts = list(map(lambda el: token_counter[el], tokens))
-# 		f.write(",".join(map(str, [id_num] + tf_counts)) + "\n")
-# 	f.close()
-
-# def tf(data):
-# 	return data
-
-# def idf(data):
-# 	N, D = data.shape
-# 	idf = np.zeros(D)
-# 	for term_idx in range(D):
-# 		# Apply the 1 + trick to avoid dividing by 0
-# 		idf[term_idx] = np.log(D / (1 + np.count_nonzero(data[:, term_idx])))
-# 	return idf
-
-# def tf_idf(data):
-# 	return tf(data) * idf(data)
-
-# def n_grams(pairs):
-# 	pass
-
-# def save_data(header, **kwargs):
-# 	for name, data in kwargs.items():
-# 		np.savetxt('data/{0}.csv'.format(name), data, delimiter=',', header=header)
-
 # Set seed for reproducability
 np.random.seed(1337)
 
-# ### INITIAL
+# TODO: Put everything into functions
+### INITIAL
 df = pandas.read_csv('data/news_ds.csv')
 # Remove HTML tags, digits, punctuation
 df['CLEAN'] = df['TEXT'].str.replace('<[^<]+?>|^\d+\s|\s\d+\s|\s\d+$|[^\w\s]', '')
 
-# # Work with a small subset
+# Work with a small subset
 num_sample = 250
-raw_data = df['CLEAN'][:num_sample]
-raw_labels = df['LABEL'][:num_sample]
+raw_data = df['CLEAN'][:]
+raw_labels = df['LABEL'][:]
 # Split ratios
 # 75% training, 25% test
 train_ratio, val_ratio = .75, .0
@@ -102,39 +49,25 @@ print("Splitting")
 # sklearn randomly removes the training set
 # X_df and X_test_df gives back the text itself
 # y_df and y_test_df gives back the labels
-# PREV. VER.
-# X_df, X_test_df, y_df, y_test_df = train_test_split(raw_data, raw_labels, test_size=test_ratio)
 X_train, X_test, y_train, y_test = train_test_split(raw_data, raw_labels, test_size=test_ratio, random_state=1337)
-
-# Use sklearn to randomly pick validation set
-# PREV. VER.
-# X_train_df, X_val_df, y_train_df, y_val_df = train_test_split(X_df, y_df, test_size = val_ratio/(1-test_ratio))
 
 print("Counting")
 
-# Identify all unique words in training set
-# unique_tokens = identify_tokens(X_train_df.items())
-# X_train, X_val, X_test = map(lambda partition: count_tokens(partition.items(), unique_tokens), [X_train_df, X_val_df, X_test_df])
-# y_train, y_val, y_test = map(lambda partition: np.array([x[1] for x in partition.items()]), [y_train_df, y_val_df, y_test_df])
-
 # Apply transformations
-# X_train_tf, X_val_tf, X_test_tf = map(lambda partition: tf_idf(partition), [X_train, X_val, X_test])
-
-tf = TfidfVectorizer(input='content', lowercase=True, analyzer='word', ngram_range=(2,4), min_df=0, stop_words=STOP_WORDS, tokenizer=LemmaTokenizer())
+tf = TfidfVectorizer(input='content', lowercase=True, analyzer='word', ngram_range=(3,5), min_df=0, stop_words=STOP_WORDS, tokenizer=LemmaTokenizer())
 X_train_dtm = tf.fit_transform(X_train)
 X_test_dtm = tf.transform(X_test)
 
 print("Fitting")
+
 # Fit
 clf = MultinomialNB()
 clf.fit(X_train_dtm, y_train)
 
 print("Predicting")
 
-# Check validation score
-# TODO: Check if positives means fake(0) or real(1)
+# Positive means real(1)
 y_pred = clf.predict(X_test_dtm)
-# cf_matrix = confusion_matrix(y_pred, y_val)
 # tn, fn, fp, tp = cf_matrix.flatten()
 # accuracy = (tp + tn) / (tn + fp + fn + tp)
 # precision = tp / (tp + fp)
@@ -148,8 +81,3 @@ F1 = met.f1_score(y_test, y_pred)
 null_accuracy = y_test.value_counts().head(1) / len(y_test)
 print("Accuracy: {0}, Precision: {1}, Recall: {2}, F1: {3}, \nNull Accuracy: {4}".format(accuracy, precision, recall, F1, null_accuracy))
 
-# print("Saving")
-# save_tokens(raw_data.items(), tokens, "data/X_tokens.csv")
-
-# ### WITH TOKENS
-# X_tokens = pandas.read_csv("data/X_tokens.csv")
