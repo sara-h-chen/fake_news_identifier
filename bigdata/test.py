67db48c676e6ad83fcc7229ed54325e6c5ad68a6
sara-h-chen <deutscher71112@gmail.com>
2018-03-09 21:12:38 +0000

    Begin rewriting CSV parsing

---
+++ bigdata/test.py
@@ -0,0 +1,207 @@
+import pandas
+import csv
+import numpy as np
+import spacy
+import time
+import re
+
+from spacy.lang.en.stop_words import STOP_WORDS
+
+import sklearn.metrics as met
+from sklearn.model_selection import train_test_split
+
+from keras import optimizers
+from keras.callbacks import Callback
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers import Embedding
+from keras.preprocessing.sequence import pad_sequences
+
+# Load spaCy once
+nlp = spacy.load('en_core_web_md')
+
+index_dict = {}
+word_dictionary = {}
+
+########################################################
+#                METRICS CALLBACK CLASS                #
+########################################################
+# Wrap scikit metrics into a class because Keras can   #
+# only provide these metrics as a callback function.   #
+########################################################
+
+class Metrics(Callback):
+    def on_train_begin(self, logs={}):
+        self.confusion = []
+        self.precision = []
+        self.recall = []
+        self.f1s = []
+        self.accuracy = []
+
+    def on_epoch_end(self, epoch, logs={}):
+        score = np.asarray(self.model.predict(self.validation_data[0]))
+        predict = np.round(np.asarray(self.model.predict(self.validation_data[0])))
+        targ = self.validation_data[1]
+
+        self.confusion.append(met.confusion_matrix(targ, predict))
+        self.precision.append(met.precision_score(targ, predict))
+        self.recall.append(met.recall_score(targ, predict))
+        self.f1s.append(met.f1_score(targ, predict))
+        self.accuracy.append(met.accuracy_score(targ, predict))
+        print("Precision: {0}, Recall: {1}, F1 Score: {2},\nAccuracy: {3}".format(self.precision[-1],
+                                                                                  self.recall[-1],
+                                                                                  self.f1s[-1],
+                                                                                  self.accuracy[-1]))
+        return
+
+
+########################################################
+#                   LEMMA TOKENIZER                    #
+########################################################
+# Takes the base form of every word, and keeps it only #
+# if it's a valid word or phrase:                      #
+# https://stackoverflow.com/questions/45196312/spacy   #
+# -and-scikit-learn-vectorizer                         #
+########################################################
+
+class LemmaTokenizer(object):
+    def __init__(self):
+        self.spacynlp = nlp
+
+    def __call__(self, doc):
+        nlpdoc = self.spacynlp(doc)
+        nlpdoc = [token.lemma_ for token in nlpdoc if (len(token.lemma_) > 1) or (token.lemma_.isalnum())]
+        return nlpdoc
+
+
+########################################################
+#                   WORD VECTORIZER                    #
+########################################################
+# Uses spaCy's inbuilt pre-trained word2vec model      #
+########################################################
+
+def create_embedding_matrix(bag_of_words):
+    embedding_matrix = np.zeros((len(bag_of_words.keys()), 300))
+    for word, vector in bag_of_words.items():
+        index_tracker = index_dict[word]
+        embedding_matrix[index_tracker] = vector
+    return embedding_matrix
+
+
+def string_to_wordvec(string, ctr):
+    returned_sentence = []
+    for token in string:
+        if token in nlp.vocab:
+            word_dictionary[token] = nlp(token).vector
+        if token not in index_dict:
+            index_dict[token] = ctr
+            ctr += 1
+        returned_sentence.append(index_dict[token])
+    return returned_sentence
+
+
+def vectorize_words(pairs, label_pairs, index):
+    vectorized_words = []
+    # List containing ((id, string), (id, label))
+    label_after_vec = [x[1] for x in label_pairs]
+    for string in pairs:
+        vectorized_words.append(string_to_wordvec(string, index))
+    return vectorized_words, label_after_vec
+
+
+# Transform words into tokens
+def transform_words(x_dataframe, y_dataframe, counter):
+    data, labels = vectorize_words(x_dataframe, y_dataframe, counter)
+    # Truncate longer to 1000 and pad shorter to 1000
+    data = pad_sequences(data, maxlen=1000, padding='post')
+    return data, labels
+
+
+########################################################
+#                  INPUT SANITIZER                     #
+########################################################
+# Sanitize: remove HTML tags, digits, punctuation
+# Remove stop words
+def read_input(path_to_csv):
+    dataframe = pandas.read_csv(path_to_csv)
+    dataframe['CLEAN'] = dataframe['TEXT'].str.lower().str.replace('<[^<]+?>|^\d+\s|\s\d+\s|\s\d+$|[^\w\s]|^https?:\/\/.*[\r\n]*', '')
+    dataframe['SPLIT'] = dataframe['CLEAN'].str.split()
+    dataframe['SPLIT'] = dataframe['SPLIT'].apply(lambda x: [item for item in x if item not in STOP_WORDS])
+    # print(dataframe['SPLIT'])
+    return dataframe
+
+
+########################################################
+#              LSTM MODEL IMPLEMENTATION               #
+########################################################
+# Create model
+def create_model(timesteps, dimensions, train_data, train_labels, val_data, val_labels, mtrcs, embedding_matrix):
+    model = Sequential()
+    model.add(Embedding(timesteps, 300, weights=[embedding_matrix], input_length=1000))
+    model.add(LSTM(20, input_shape=(timesteps, dimensions),
+                   activation='relu'))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(LSTM(10, input_shape=(timesteps, dimensions), activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    # Loss is t * log(y) + (1 - t) * log (1 - y)
+    # sgd = optimizers.SGD(lr=0.01, clipvalue=0.3)
+    adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-3, decay=0.0, amsgrad=True)
+
+    model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+    print("Fitting")
+    model.fit(train_data, train_labels, epochs=3, batch_size=5, verbose=1,
+              validation_data=[val_data, val_labels], callbacks=[mtrcs])
+    scores = model.evaluate(X_val, y_val, verbose=0)
+    print('Accuracy on validation: %.5f' % (scores[1] * 100))
+    return model
+
+
+########################################################
+#                     MAIN METHOD                      #
+########################################################
+
+if __name__ == '__main__':
+    # SETUP
+    # Set seed for reproducability
+    np.random.seed(1337)
+    metrics = Metrics()
+
+    t0 = time.time()
+
+    print("Reading data")
+    dir_to_data = 'data/news_ds.csv'
+
+    df = read_input(dir_to_data)
+
+    # Work with a small subset
+    num_sample = 50
+    raw_data = df['SPLIT'][:num_sample]
+    raw_labels = df['LABEL'][:num_sample]
+
+    counter = 0
+    data, labels = transform_words(raw_data, raw_labels, counter)
+
+    print("Reading: ", time.time() - t0, "seconds wall time")
+
+    t0 = time.time()
+    # Split ratios
+    train_ratio, val_ratio = .7, .2
+    test_ratio = 1 - train_ratio - val_ratio
+    print("Splitting")
+    X, X_test, y, y_test = train_test_split(data, labels, test_size=test_ratio, random_state=1337)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=val_ratio / (1 - test_ratio), random_state=1337)
+
+    e_matrix = create_embedding_matrix(word_dictionary)
+
+    print("Splitting and creating embedding matrix: ", time.time() - t0, "seconds wall time")
+
+    num_timesteps, num_dimensions = e_matrix.shape
+
+    lstm_model = create_model(num_timesteps, num_dimensions, train_data, train_labels, val_data, val_labels, metrics, e_matrix)
+
+    # print("Predicting")
+    # TODO: Implement testing
