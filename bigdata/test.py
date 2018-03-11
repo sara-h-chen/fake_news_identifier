17ca2c6d6718404a7a7e9c6195eeba71e51df2a0
sara-h-chen <deutscher71112@gmail.com>
2018-03-11 17:11:00 +0000

    Finish rewriting encoding

--- bigdata/test.py
+++ bigdata/test.py
@@ -1,4 +1,3 @@
-import argparse
 import pandas
 import csv
 import numpy as np
@@ -6,6 +5,9 @@ import spacy
 import time
 import re
 import json
+import operator
+
+from collections import Counter
 
 from spacy.lang.en.stop_words import STOP_WORDS
 
@@ -23,13 +25,11 @@ from keras.layers import Embedding
 from keras.preprocessing.sequence import pad_sequences
 
 # Load spaCy once
-nlp = spacy.load('en_core_web_lg')
+nlp = spacy.load('en_core_web_md')
 
-vocabulary = set()
-# index_dict = {}
-# word_dictionary = {}
-# Keep track of transformed articles to pickle
-# transformed_articles = {}
+# vocabulary = set()
+common_words = set()
+counter = Counter()
 
 ########################################################
 #                METRICS CALLBACK CLASS                #
@@ -55,7 +55,7 @@ class Metrics(Callback):
         self.precision.append(met.precision_score(targ, predict))
         self.recall.append(met.recall_score(targ, predict))
         self.f1s.append(met.f1_score(targ, predict))
-        self.accuracy.append(met.accuracy_score(targ, predict.round()))
+        self.accuracy.append(met.accuracy_score(targ, predict))
         print("\nPrecision: {0}, Recall: {1}, F1 Score: {2},\nAccuracy: {3}".format(self.precision[-1],
                                                                                   self.recall[-1],
                                                                                   self.f1s[-1],
@@ -88,41 +88,61 @@ class LemmaTokenizer(object):
 # Uses spaCy's inbuilt pre-trained word2vec model      #
 ########################################################
 
-# def create_embedding_matrix():
-#     embedding_matrix = np.zeros((len(vocabulary), 300))
-#     for word in vocabulary: 
-#         embedding_matrix[index_tracker] = vector
-#     return embedding_matrix
+def create_embedding_matrix(label_encoder, max_valid, sorted_list):
+    embedding_matrix = np.zeros((len(common_words), 300))
+    for word, appearances in sorted_list:
+        index = label_encoder.transform([word])
+        embedding_matrix[index] = nlp(word).vector
+    return embedding_matrix
 
 
-def string_to_wordvec(string, label_encoder, embedding_matrix):
-    transformed_string = label_encoder.transform(string)
-    for token in transformed_string:
-        # if token is unseen
-        embedding_matrix[token] = nlp(label_encoder.inverse_transform([token])).vector
-    return transformed_string
+def string_to_wordvec(string, label_encoder):
+    string = [x for x in string if x in common_words]
+    transformed_sentence = label_encoder.transform(string)
+    return transformed_sentence
 
 
-def vectorize_words(pairs, label_pairs, index):
-    vectorized_words = []
-    e_matrix = np.zeros((len(vocabulary), 300))
-    # List containing ((id, string), (id, label))
+def build_common_words_set(max_size):
+    sorted_occurrences = list(sorted(counter.items(), key=operator.itemgetter(1), reverse=True))
+    sorted_occurrences = sorted_occurrences[:max_size]
+    common_words.update([x[0] for x in sorted_occurrences])
+    return sorted_occurrences
+
+
+def vectorize_words(pairs, label_pairs, max_valid_words):
+    sorted_words = build_common_words_set(max_valid_words)
+
     le = LabelEncoder()
-    le.fit(list(vocabulary))
-    for string in pairs:
-        vectorized_words.append(string_to_wordvec(string, le, e_matrix))
-        # DEBUG: Take this out when not pickling
+    transformed_word_bag = le.fit(list(common_words))
+
+    embedding_matrix = create_embedding_matrix(le, max_valid_words, sorted_words)
+    vectorized_words = []
+    # List containing ((id, string), (id, label))
+    article_count = 0
+    for string_list in pairs:
+        transformed = string_to_wordvec(string_list, le)
+        vectorized_words.append(transformed)
         print("Article %d transformed" % article_count)
         article_count += 1
-    return vectorized_words, [x for x in label_pairs], e_matrix
+    return vectorized_words, label_pairs, embedding_matrix
 
 
 # Transform words into tokens
-def transform_words(x_dataframe, y_dataframe, counter):
-    data, labels, embedding_mtrx = vectorize_words(x_dataframe, y_dataframe, counter)
+def transform_words(x_dataframe, y_dataframe, max_words):
+    data, labels, embed_matrix = vectorize_words(x_dataframe, y_dataframe, max_words)
     # Truncate longer to 1000 and pad shorter to 1000
-    data = pad_sequences(data, maxlen=1000, padding='post')
-    return data, labels, embedding_mtrx
+    data = pad_sequences(data, maxlen=1000, padding='pre')
+    return data, labels, embed_matrix
+
+
+########################################################
+#                   HELPER FUNCTIONS                   #
+########################################################
+
+def increment_count(list_of_words):
+    for word in list_of_words:
+        counter[word] += 1
+    return
 
 
 ########################################################
@@ -130,13 +150,17 @@ def transform_words(x_dataframe, y_dataframe, counter):
 ########################################################
 # Sanitize: remove HTML tags, digits, punctuation
 # Remove stop words
+# Find vocabulary set
 def read_input(path_to_csv):
     dataframe = pandas.read_csv(path_to_csv)
-    dataframe['CLEAN'] = dataframe['TEXT'].str.lower().str.replace('<[^<]+?>|^\d+\s|\s\d+\s|\s\d+$|[^\w\s]|^https?:\/\/.*[\r\n]*', '')
+    dataframe['CLEAN'] = dataframe['TEXT'].str.lower().str.replace('<[^<]+?>|\d|[^\w\s]|^https?:\/\/.*[\r\n]*|\n', '')
+    dataframe = dataframe.replace(r'^\s*$', np.nan, regex=True)
+    dataframe = dataframe.dropna()
     dataframe['SPLIT'] = dataframe['CLEAN'].str.split()
     dataframe['SPLIT'] = dataframe['SPLIT'].apply(lambda x: [item for item in x if item not in STOP_WORDS])
-    dataframe['SPLIT'] = dataframe['SPLIT'].apply(lambda x: [vocabulary.add(item) for item in x if item not in vocabulary])
-    print(dataframe['SPLIT'])
+    dataframe['SPLIT'].apply(lambda x: increment_count(x))
+    # dataframe['SPLIT'].apply(lambda x: vocabulary.update(x))
+    # print(dataframe['SPLIT'])
     return dataframe
 
 
@@ -145,36 +169,31 @@ def read_input(path_to_csv):
 ########################################################
 # Create model
 def create_model(timesteps, dimensions, train_data, train_labels, val_data, val_labels, mtrcs, embedding_matrix):
+    embedding_layer = Embedding(output_dim=300, input_dim=embedding_matrix.shape[0], trainable=False)
+    embedding_layer.build((None,))
+    embedding_layer.set_weights([embedding_matrix])
+
     model = Sequential()
-    model.add(Embedding(timesteps, 300, weights=[embedding_matrix], input_length=1000, trainable=False))
-    model.add(LSTM(32, return_sequences=True))
-    model.add(Dropout(0.02))
-    model.add(LSTM(32))
+    model.add(embedding_layer)
+    model.add(LSTM(20, input_shape=train_data.shape[1:], return_sequences=True, activation='tanh'))
+    model.add(Dropout(0.05))
+    model.add(LSTM(10, activation='tanh'))
     model.add(Dense(1, activation='sigmoid'))
     # Loss is t * log(y) + (1 - t) * log (1 - y)
     # sgd = optimizers.SGD(lr=0.01, clipvalue=0.3)
     model.summary()
-    # adam = optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-8, decay=0.0, amsgrad=True)
-    rmsprop = optimizers.RMSprop()
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    adam = optimizers.Adam(lr=0.0003, beta_1=0.9, beta_2=0.999, epsilon=1e-3, decay=0.0, amsgrad=True)
+    # rmsprop = optimizers.RMSprop(lr=0.001)
+    model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
 
     # print(index_dict)
     # print(val_data, val_labels)
     print("Fitting")
-    model.fit(train_data, train_labels, epochs=10, batch_size=64, verbose=1,
+    model.fit(train_data, train_labels, epochs=10, batch_size=16, verbose=1,
               validation_data=(val_data, val_labels), callbacks=[mtrcs])
     scores = model.evaluate(val_data, val_labels, verbose=0)
     print('Accuracy on validation: %.5f' % (scores[1] * 100))
     return model
-
-
-########################################################
-#                  HELPER FUNCTIONS                    #
-########################################################
-
-# def read_json():
-#     with open("pickled_data.json", 'r') as f:
-#         reconstructed_summaries = np.zeroes((
 
 
 ########################################################
@@ -186,18 +205,12 @@ if __name__ == '__main__':
     # Set seed for reproducability
     np.random.seed(1337)
     metrics = Metrics()
-    parser = argparse.ArgumentParser()
-    # TODO: Add argument parser params
-    args = parser.parse_args()
 
     t0 = time.time()
 
     print("Reading data")
     dir_to_data = 'data/news_ds.csv'
 
-    # if args.read:
-    #     df = pandas.read_csv('dumped_df.csv')
-    # else:
     df = read_input(dir_to_data)
 
     # Work with a small subset
@@ -205,21 +218,9 @@ if __name__ == '__main__':
     raw_data = df['SPLIT'][:num_sample]
     raw_labels = df['LABEL'][:num_sample]
 
-    # if args.dump:
-    # 	df.to_csv('dumped_df.csv', sep=',', encoding='utf-8', index=False)
-
-    counter = 0
-    data, labels, e_matrix = transform_words(raw_data, raw_labels, counter)
-
-    # with open('pickled_data.json', 'w') as outfile:
-    #     json.dump(data, outfile)
-    # with open('picked_labels.json', 'w') as outfile:
-    #     json.dump(labels, outfile)
-    # with open('pickled_indices.json', 'w') as outfile:
-    #     serializable_dict = {k: v.tolist() for k, v in word_dictionary.items()}
-    #     json.dump(serializable_dict, outfile)
-    # with open('pickled_words.json', 'w') as outfile:
-    #     json.dump(index_dict, outfile)
+    # e_matrix = create_embedding_matrix(word_dictionary)
+    max_recognized_words = 5000
+    data, labels, e_matrix = transform_words(raw_data, raw_labels, max_recognized_words)
 
     print("Reading: ", time.time() - t0, "seconds wall time")
 
@@ -228,10 +229,9 @@ if __name__ == '__main__':
     train_ratio, val_ratio = .7, .2
     test_ratio = 1 - train_ratio - val_ratio
     X, X_test, y, y_test = train_test_split(data, labels, test_size=test_ratio, random_state=1337)
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=val_ratio / (1 - test_ratio), random_state=1337)
-    print("Splitting into %d train, %d validation and %d test" % (len(X_train), len(X_val), len(X_test)))
 
-    # e_matrix = create_embedding_matrix(word_dictionary)
+    X_train, X_val, y_train, y_val = train_test_split(data, labels, test_size=val_ratio / (1 - test_ratio), random_state=1337)
+    print("Splitting into %d train, %d validation and %d test" % (len(X_train), len(X_val), len(X_test)))
 
     print("Splitting and creating embedding matrix: ", time.time() - t0, "seconds wall time")
 
